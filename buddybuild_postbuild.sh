a62618d6ef5403250432bd078b7caee38a5e6dd6
Romain Pouclet <romain.pouclet@gmail.com>
2017-05-10 18:25:12 -0400

    Update script

--- buddybuild_postbuild.sh
+++ buddybuild_postbuild.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 
+set -e
+
 bundle install 
+echo "Running tests for scheme $BUDDYBUILD_SCHEME"
+
 xcodebuild -scheme $BUDDYBUILD_SCHEME -project Paparazzi.xcodeproj test | bundle exec xcpretty -t
